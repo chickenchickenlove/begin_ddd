d8ec61826a04583fda8d834fab602b28dff132f5
chickenchickenlove <ojt90902@naver.com>
2023-10-15 15:34:51 +0900

    git init

---
+++ dddstart/src/test/java/com/me/dddstart/DddstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.dddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
